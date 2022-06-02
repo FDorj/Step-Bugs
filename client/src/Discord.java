214016b0a173f8df02d2c31f15660a6f7fc2479d
FDorj <f.dorj2@gmail.com>
2022-06-01 17:38:47 -0800

    feat: some classes edited

--- client/src/Discord.java
+++ client/src/Discord.java
@@ -1,7 +1,34 @@
 import java.util.HashSet;
 
 public class Discord {
-    private HashSet<User> allUsers;
+//    private HashSet<User> allUsers;
 
+    public void signUp (User user) {
+
+    }
+
+    public void signIn (String userName , String password) {
+
+    }
+
+    public void checkUserName (String userName) {
+
+    }
+
+    public void checkPassword (String password) {
+
+    }
+
+    public void checkEmail (String email) {
+
+    }
+
+    public void saveImage () {
+
+    }
+
+    public void friendRequest (User user) {
+
+    }
 
 }
--- client/src/User.java
+++ client/src/User.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 
 public class User {
     private String userName;
-    private String password;
+//    private String password;
     private String hashedPassword;
     private String email;
     private String phoneNumber;
@@ -12,6 +12,23 @@ public class User {
     private HashSet<User> friends;
     private HashSet<User> blocked;
     private HashMap<User, PrivateChat> chats;
+
+    public User(String userName, String hashedPassword, String email, String phoneNumber) {
+        this.userName = userName;
+        this.hashedPassword = hashedPassword;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.status = null;
+        this.friends = new HashSet<>();
+        this.blocked = new HashSet<>();
+        this.chats = new HashMap<>();
+    }
+
+    public void printFriends() {
+        for (User user : friends) {
+            System.out.println(user + "\n");
+        }
+    }
 
 
 }
